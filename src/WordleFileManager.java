696b4fd0b34adb43bd15e47d2e5c760d70b089a2
Charlie <charliedavidcrane13@gmail.com>
2024-12-15 18:46:08 +0100

    Added method in file manager to read words from file into array

--- src/WordleFileManager.java
+++ src/WordleFileManager.java
@@ -1,7 +1,53 @@
+import java.io.BufferedReader;
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * Clase encargado de leer y escribir al fichero de palabras y de juegos hechos
+ * @author Charles Crane
+ * @version 1.0
+ */
 public class WordleFileManager {
 
-    // This class contains code to read from the words file, turn it into an array
+    /**
+     * Lee un fichero de palabras, los convierte a mayusculas y las almacena en un array
+     * @return Un array de palabras
+     */
+    public static String[] readFileWords(){
 
-    // Also it has to log every try the user does into another file
+        // Utilizando un arraylist porque no sabemos cuantas palabras hay en el fichero
+        List<String> wordsList = new ArrayList<String>();
+
+        try
+        {
+            File file = new File("C:\\Users\\charl\\wordle\\src\\palabras.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                wordsList.add(line);
+            }
+        }
+        catch (Exception e)
+        {
+
+            e.printStackTrace();
+
+        }
+
+        // Convertir ArrayList a un array ya que sabemos el numero de palabras
+        String[] palabras = wordsList.toArray(new String[wordsList.size()]);
+
+        // Hacer todas palabras mayusculas
+        for(int i = 0; i < palabras.length; i++){
+            palabras[i] = palabras[i].toUpperCase();
+        }
+
+        return palabras;
+
+
+    }
 
 }
